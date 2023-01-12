65a3db46b481464f31140f395e1313096d7be5b3
2Space9 <sjk338285@gmail.com>
2023-01-12 20:23:49 +0900

    2023-1-12

---
+++ C++/1008.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    
+    double A;
+    double B;
+ 
+    cin >> A;
+    cin >> B;
+    
+    cout.precision(10);
+    cout << A / B;
+    return 0;
+}
