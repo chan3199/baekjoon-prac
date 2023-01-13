3163603aca9438a86daf3d9a27ea02df5b2ea070
sjk338285@naver.com <sjk338285@gmail.com>
2023-01-13 19:46:46 +0900

    2023-1-13-2

---
+++ C++/10807.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int n, v;
+int arr[101];
+
+int main()
+{
+	int r = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+	cin >> v;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] == v) {
+            r++;
+        }
+    } 
+	cout << r ;
+}
