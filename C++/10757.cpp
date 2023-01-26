c502a5ef9c826fa00fc108e1af630ccedeb24eaf
sjk338285@naver.com <sjk338285@gmail.com>
2023-01-26 23:04:36 +0900

    2023-01-26-1

---
+++ C++/10757.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    long long int A,B,C;
+
+	cin >> A >> B;
+
+    C = A + B;
+
+    cout << C;
+
+}
---
+++ C++/2839.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int N;
+
+	cin >> N;
+
+	int result = 0;
+
+	while ( N >= 0 ) {
+		if (N % 5 == 0) {
+			result += N / 5;	
+			cout << result;
+			return 0;
+		}
+		N -= 3;	
+		result++;	
+	}
+	cout << -1;
+}
